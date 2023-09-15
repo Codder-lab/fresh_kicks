15978ba2b2fec6858ced0c70a9bd9faec0ea65bb
Codder-lab <84835929+Codder-lab@users.noreply.github.com>
2023-09-15 14:29:03 +0000

    Create order database

--- models/order.js
+++ models/order.js
@@ -10,10 +10,38 @@ const orderSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Product',
     }],
-    shippingAddress: {
+    firstName: {
         type: String,
         required: true,
     },
+    lastName: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        validate(value) {
+            if(!validator.isEmail(value)) {
+                throw new Error("Email is invalid")
+            }
+        }
+    },
+    address: {
+        type: String,
+        required: true,
+    },
+    state: {
+        type: String,
+        required: true,
+    },
+    zip: {
+        type: Number,
+        required: true,
+    },
+        
     // Add other order-related fields such as total price, order date, status, etc.
 }, {
     timestamps: true,
